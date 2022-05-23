9724daa790d6491c3d67a769badb280eaf03b42a
JonathanAmador12 <jamadorh91@gmail.com>
2022-05-22 20:04:16 -0500

    Initial Commit

---
+++ TripApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TripApp
+//
+//  Created by JonathanA on 22/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TripApp/TripAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripAppApp.swift
+//  TripApp
+//
+//  Created by JonathanA on 22/05/22.
+//
+
+import SwiftUI
+
+@main
+struct TripAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
